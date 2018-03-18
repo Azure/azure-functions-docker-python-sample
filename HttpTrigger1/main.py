e5d05cd15e84be1a4561ef1a56fd1156bf83193c
Asavari Tayal <asavaritayal@LEIHEPC.fareast.corp.microsoft.com>
2018-03-17 17:26:19 -0700

    Updating Http template

--- HttpTrigger1/main.py
+++ HttpTrigger1/main.py
@@ -1,5 +1,24 @@
-import azure.functions
+import logging
 
-def main(req: azure.functions.HttpRequest) -> str:
-    user = req.params.get('user', 'User')
-    return f'Hello, {user}!'
+import azure.functions as azf
+
+
+def main(req: azf.HttpRequest) -> azf.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return azf.HttpResponse(f"Hello {name}!")
+    else:
+        return azf.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
